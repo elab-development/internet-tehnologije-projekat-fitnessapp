23991d6f4f449beb8592d1e25ceb70a95e399387
JelenaVujin <persephone1805@gmail.com>
2024-04-17 14:32:20 +0200

    dodat trainer controller, resource i collection

---
+++ domaci2/domaci2fitness/app/Http/Resources/TrainerResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TrainerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=>$this->resource->id,
+            'name'=>$this->resource->name,
+            'licence_number'=>$this->resource->licence_number,
+            'email'=>$this->resource->email
+        ];
+    }
+}
