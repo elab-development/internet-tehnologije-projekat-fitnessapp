a67877daf17d831ac9d45a51315a91bbebfd67ff
JelenaVujin <persephone1805@gmail.com>
2024-07-13 17:28:28 +0200

    omogucen search u planovima

--- domaci3/domaci3react/src/components/MyWorkoutPlans.jsx
+++ domaci3/domaci3react/src/components/MyWorkoutPlans.jsx
@@ -1,41 +1,65 @@
 import React from "react";
 import { useEffect, useState, useParams } from "react";
 import axios from "axios";
-import { debounce } from 'lodash';
+import { debounce } from "lodash";
 import useWorkouts from "./useWorkouts";
 function MyWorkoutPlans() {
   const [reservations, setReservations] = useState([]);
   const [filteredReservations, setFilteredReservations] = useState([]);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   useEffect(() => {
-    const token = sessionStorage.getItem('authToken');
+    const token = sessionStorage.getItem("authToken");
     const config = {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     };
 
-    axios.get('api/myWorkouts', config)
+    axios
+      .get("api/myWorkouts", config)
       .then((response) => {
         setReservations(response.data.data);
-        
-        
+        setFilteredReservations(response.data.data);
       })
       .catch((err) => {
         console.log("Error");
-        
       });
   }, []);
 
-  
-  
+  const searchReservations = (text) => {
+    setSearchText(text);
+    const lowercasedText = text.toLowerCase();
+    const filtered = reservations.filter((reservation) => {
+      return searchInObject(reservation, lowercasedText);
+    });
+    setFilteredReservations(filtered);
+  };
+  const debouncedSearch = debounce(searchReservations, 300);
 
-  
+  const handleSearchChange = (e) => {
+    debouncedSearch(e.target.value);
+  };
+  const searchInObject = (obj, searchText) => {
+    if (obj === null) return false;
+    if (typeof obj === "object") {
+      return Object.values(obj).some((value) =>
+        searchInObject(value, searchText)
+      );
+    }
+    return String(obj).toLowerCase().includes(searchText);
+  };
 
   return (
-    
+    <div>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Search..."
+          onChange={handleSearchChange}
+        />
+      </div>
       <table class="table">
-       
         <thead>
           <tr>
             <th scope="col">Workout type</th>
@@ -46,19 +70,18 @@ function MyWorkoutPlans() {
           </tr>
         </thead>
         <tbody>
-        {reservations.map((reservation) => (
-          <tr>
-             <td>{reservation.workout.title}</td>
-             <td>{reservation.date}</td>
-             <td>{reservation.time}</td>
-             <td>{reservation.trainer.name}</td>
-             <td>{reservation.gym.name}</td>
-             
-             </tr>
+          {filteredReservations.map((reservation) => (
+            <tr>
+              <td>{reservation.workout.title}</td>
+              <td>{reservation.date}</td>
+              <td>{reservation.time}</td>
+              <td>{reservation.trainer.name}</td>
+              <td>{reservation.gym.name}</td>
+            </tr>
           ))}
         </tbody>
       </table>
-    
+    </div>
   );
 }
 
